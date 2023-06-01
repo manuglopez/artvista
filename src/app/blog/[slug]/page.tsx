5871d12c7f2cc4ccb1bbb70c1f9067b80717885a
manuglopez <darkwebside@gmail.com>
2023-06-01 11:11:45 +0200

    modified padding in single post page

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -7,8 +7,8 @@ import Image from 'next/image'
 import path from "path";
 
 const getPostContent = (slug: string) => {
-    const folder= path.join(path.resolve('src/posts/'),'/')
-   /* const folder = 'src/posts/'*/
+    const folder = path.join(path.resolve('src/posts/'), '/')
+    /* const folder = 'src/posts/'*/
     const file = `${folder}${slug}.md`
     const content = fs.readFileSync(file, 'utf-8')
     const matterResult = matter(content)
@@ -27,13 +27,17 @@ const PostPage = (props: any) => {
     const slug = props.params.slug
     const article = getPostContent(slug)
     return (
-        <div className='max-w-2/3 flex flex-col items-center justify-center'>
-            <h1 className='text-5xl text-slate-500 font-light py-12'>{article.data.title}</h1>
-            <Image className='rounded-md shadow-lg my-8 ' src={`/images/${slug}.jpg`} width={1024} height={768} alt={article.data.subtitle} />
-            <article className='prose lg:prose-xl'>
-                <Markdown>{article.content}</Markdown>
-            </article>
-        </div>
+        <>
+            <div className='max-w-2/3 flex flex-col items-center justify-center px-6 sm:px-14'>
+                <h1 className='text-5xl  text-slate-500 font-light py-12'>{article.data.title}</h1>
+                <Image className='rounded-md shadow-lg my-8 ' src={`/images/${slug}.jpg`} width={1024} height={768}
+                       alt={article.data.subtitle}/>
+                <article className='prose lg:prose-xl'>
+                    <Markdown>{article.content}</Markdown>
+                </article>
+            </div>
+        </>
+
     )
 }
 
